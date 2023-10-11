62d9ac4c7d87371023230b7138d58442ab827f79
Edwin <edwin12sv@gmail.com>
2023-10-10 23:48:29 -0600

    Initial Commit

---
+++ ControlsAndTextViews/ControlsAndTextViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControlsAndTextViewsApp.swift
+//  ControlsAndTextViews
+//
+//  Created by elsalvador on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ControlsAndTextViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
